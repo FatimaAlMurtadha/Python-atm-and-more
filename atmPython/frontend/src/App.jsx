3b7eaaa1c19e55638b11836bc79c6013d309c246
FatimaAlmurtadha <sukaina77150@gmail.com>
2026-04-29 06:20:18 +0200

    Fetching card api

--- atmPython/frontend/src/App.jsx
+++ atmPython/frontend/src/App.jsx
@@ -1,11 +1,26 @@
 import './App.css'
+import { useState } from "react";
+import WelcomeScreen from "./components/WelcomeScreen";
 
 function App() {
+  const [state, setState] = useState("IDLE");
+
+  async function handleInsertCard() {
+    const respons = await fetch("http://localhost:8000/insert_card", {
+      method: "POST",
+      headers: {
+        body: JSON.stringify({ card_number: "1234-5678" })
+      }
+    });
+    const data = await respons.json();
+    console.log(data);
+    setState(data.state);
+  }
 
   return (
     <>
-      <title>Atm Machine</title>
-      <h1>Welcome Fatima</h1>
+      {state === "IDLE" && <WelcomeScreen onInsert={handleInsertCard} />}
+      {state === "CARD_INSERTED" && <h2>Card Inserted. Please enter your PIN.</h2>}
     </>
   )
 }
