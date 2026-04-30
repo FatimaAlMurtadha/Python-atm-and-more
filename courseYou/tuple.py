b2260d6ca6ea5cb354164ffb2a1f09f0e07723e4
FatimaAlmurtadha <sukaina77150@gmail.com>
2026-04-30 08:21:40 +0200

    Tuples

---
+++ courseYou/tuple.py
@@ -0,0 +1,43 @@
+# Tuple: ordered, immutable, allows duplicate elements
+print("="*7)
+print()
+# --------------------------------------------------------------
+
+myTuple = ("MAx", 28, "Boston")
+
+print(myTuple)
+
+# index
+# change by index
+# print with for loop
+# if in the tuple
+# len
+# myTuple.count('t')
+# myTuple.index('t')
+# Change to a list my_list = list(myTuple)
+# myTuple2 = tuple(my_list)
+# Slice
+# 
+print("="*4, ""*2, " Every elemnt seperatly  ","="*4, ""*2)
+print()
+name, age , city = myTuple
+print(name)
+print(age)
+print(city)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("="*7)
+print()
+# --------------------------------------------------------------
